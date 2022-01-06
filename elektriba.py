3efea3c65691f6752456b8a5489133eabc84d220
Ance Spolite <97155723+ancespp@users.noreply.github.com>
2022-01-06 07:33:32 +0000

    izpildīju 9. uzdevumu

---
+++ elektriba.py
@@ -0,0 +1,11 @@
+def tet(g,t):
+  if g==1:
+    eiro=t*0.20159
+  elif g==2:
+    eiro=t*0.16961
+  elif g==3:
+    eiro=t*0.16427
+  else:
+    eiro=t*0.15964
+  return eiro
+  
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import mod
+"""import mod
 a=float(input("Ievadi skaitli"))
 print("Skaitļa decimāldaļa ir", (mod.decimaldala(a)))
 print("Skaitļa kvadrāts ir",(mod.kvadrats(a)))
 print("Skaitļa kvadrātsakne ir", (mod.kvadratsakne(a)))
-print("Skaitļa zīme ir", (mod.zime(a)))
+print("Skaitļa zīme ir", (mod.zime(a)))"""
+import elektriba
+gadi=int(input("Ievadi, uz cik gadiem ir jūsu līgums "))
+tagadejais=float(input("Ievadi esošo skaitītāja rādījumu"))
+print("Jāmaksā ir", (elektriba.tet(gadi, tagadejais)))
